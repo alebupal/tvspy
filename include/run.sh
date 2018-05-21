3df796130f35e8cb88e71d6c7d2cdcc7aa43b5aa
alebupal <alebupal@gmail.com>
2018-05-21 22:53:15 +0200

    fix

--- include/run.sh
+++ include/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Iniciar crontab
-crontab /var/www/html/crontab
+crontab -u www-data /var/www/html/crontab
+service cron start
 # start db
 /etc/init.d/mysql start
-service cron reload
 # crear usuario y contraseña
 mysql -e "CREATE USER 'tvspy'@'localhost' IDENTIFIED BY 'tvspy'"
 mysql < /var/www/html/bd.sql
