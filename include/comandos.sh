c3a60d0bea847f6b2f52cc10811eee575cfe38f9
alebupal <alebupal@gmail.com>
2018-11-29 13:33:36 +0100

    dockerfile 7

--- include/comandos.sh
+++ include/comandos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #Crear base de datos
-mysql -uroot -e "create database tvspy" < /var/www/html/bd.sql
+mysql -uadmin -e "create database tvspy" 
+#< /var/www/html/bd.sql
+
 
 #cron
 ./actualizacion.sh
