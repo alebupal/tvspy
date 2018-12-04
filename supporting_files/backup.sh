34feecac63c4e9f7c9e1f920a8a72b01be276b70
alebupal <alebupal@gmail.com>
2018-12-04 22:40:57 +0100

    backup cambiado a 1 dia

--- supporting_files/backup.sh
+++ supporting_files/backup.sh
@@ -4,5 +4,5 @@ do
 	#do whatever you want, use curl, lynx to your page...
 	#php /var/www/html/acciones/actualizacion.php &>> /var/www/html/cron_log.log
 	php /var/www/html/acciones/phpBackup.php
-	sleep 1m
+	sleep 1d
 done
