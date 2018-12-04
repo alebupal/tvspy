f62d33140776baec0314fe3a00a57f924c02f1ce
alebupal <alebupal@gmail.com>
2018-12-04 23:04:36 +0100

    Versión footer

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
-			<span>alebupal - <a href="https://github.com/alebupal/tvspy" target="_blank">TVSPY 1.2.4</a> - <a href="https://www.paypal.me/alebupal" target="_blank">PayPal</a> - <a href="https://hub.docker.com/r/alebupal/tvspy/" target="_blank">Docker</a></span>
+			<span>alebupal - <a href="https://github.com/alebupal/tvspy" target="_blank">TVSPY 2.0</a> - <a href="https://www.paypal.me/alebupal" target="_blank">PayPal</a> - <a href="https://hub.docker.com/r/alebupal/tvspy/" target="_blank">Docker</a></span>
 		</div>
 	</div>
 </footer>
