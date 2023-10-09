ea4b6463490205eac36918fbc18534fffb0e5e86
Alejandro Bueno Palma <alebupal@gmail.com>
2023-10-09 22:13:48 +0200

    Añadir aplicación react integrada con la api

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+// app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+require('./database/seeder');
+
+const configRoutes = require('./routes/configRoutes'); // Importa las rutas de 'config'
+const registriesRoutes = require('./routes/registriesRoutes'); // Importa las rutas de 'registries'
+const tvheadendRoutes = require('./routes/tvheadendRoutes'); // Importa las rutas de 'tvheeadend'
+
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+// Utiliza las rutas relacionadas con 'config'
+app.use('/api', configRoutes);
+app.use('/api', registriesRoutes);
+
+app.use('/api', tvheadendRoutes);
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,26 +1,60 @@
-// app.js
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-require('./database/seeder');
-
-const configRoutes = require('./routes/configRoutes'); // Importa las rutas de 'config'
-const registriesRoutes = require('./routes/registriesRoutes'); // Importa las rutas de 'registries'
-const tvheadendRoutes = require('./routes/tvheadendRoutes'); // Importa las rutas de 'tvheeadend'
+function App() {
+    const [data, setData] = useState({});
 
 
-const app = express();
-const port = 3000;
+  useEffect(() => {
+    // URL de la API que deseas consultar
+    const apiUrl = '/api/users';
 
-app.use(bodyParser.json());
+    // Realiza la solicitud GET a la API usando Axios
+    axios.get(apiUrl)
+      .then((response) => {
+        // Almacena la respuesta de la API en el estado 'data'
+        setData(response.data.entries);
+        console.log(response.data.entries);
+      })
+      .catch((error) => {
+        console.error('Error al hacer la solicitud a la API:', error);
+      });
+  }, []);
 
-// Utiliza las rutas relacionadas con 'config'
-app.use('/api', configRoutes);
-app.use('/api', registriesRoutes);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {data.length > 0 ? (
+          <div>
+            <h2>Elementos de la API:</h2>
+            <ul>
+              {data.map((item) => (
+                <li key={item.uuid}>
+                  {item.username}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Cargando datos de la API...</p>
+        )}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-app.use('/api', tvheadendRoutes);
-
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+export default App;
