ca849822030f55cf5f1a4c366850994dcfc81fe9
Alejandro Bueno Palma <alebupal@gmail.com>
2024-08-22 18:01:11 +0200

    change path database

--- tvspy-backend/src/database/database.js
+++ tvspy-backend/src/database/database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('database.db');
+const db = new sqlite3.Database('database/database.db');
 
 // Crear la tabla 'config'
 db.run(`
