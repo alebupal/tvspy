0156455431c65911d7f1ae469441027e23746f4f
Alejandro Bueno Palma <alebupal@gmail.com>
2024-08-23 00:53:40 +0200

    fix api tvheadend

--- tvspy-backend/src/routes/tvheadendRoutes.js
+++ tvspy-backend/src/routes/tvheadendRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const axios = require('axios');
+const fetch = require('digest-fetch');
 const app = express.Router();
-const { getConfigValues } = require('./configTvheadend')
-;
+const { getConfigValues } = require('./configTvheadend');
+
 // Función para realizar una solicitud a la API externa
 const getExternalData = async (endpoint, res) => {
     try {
@@ -16,25 +16,36 @@ const getExternalData = async (endpoint, res) => {
             res
                 .status(404)
                 .json(
-                    {error: 'Algunos valores necesarios no se encontraron en la tabla de configuración'}
+                    { error: 'Algunos valores necesarios no se encontraron en la tabla de configuración' }
                 );
             return;
         }
+        
         const fullUrl = `${configValues.protocol}://${configValues.hostname}:${configValues.port}${endpoint}`;
-        const response = await axios.get(fullUrl, {
-            auth: {
+
+        const response = await fetch(fullUrl, {
+            method: 'GET',
+            digest: {
                 username: configValues.username,
-                password: configValues.password
+                password: configValues.password,
+                realm: 'tvheadend',
+                uri: endpoint,
+                algorithm: 'MD5',
+                qop: 'auth'
             }
         });
 
-        const data = response.data;
+        if (!response.ok) {
+            throw new Error('Error en la respuesta de la API externa');
+        }
+
+        const data = await response.json();
         res.json(data);
     } catch (error) {
         console.error(error);
         res
             .status(500)
-            .json({error: 'Error al consultar la API externa'});
+            .json({ error: 'Error al consultar la API externa' });
     }
 };
 
